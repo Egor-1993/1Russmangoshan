94236aabb7fe80991c0af2adc24437413184be2b
Egor-1993 <russmangosha@gmail.com>
2025-08-09 00:23:59 +0300

    arrays_and_slices 09.08.2025

--- awesomeProject1/Arrays_and_Slices/Reverse array.go
+++ awesomeProject1/Arrays_and_Slices/Reverse array.go
@@ -4,8 +4,9 @@ import "fmt"
 
 func main() {
 	var nums = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println("Array in revers order:")
+	reversed := make([]int, 0, len(nums))
 	for i := len(nums) - 1; i >= 0; i-- {
-		fmt.Print(nums[i], ",")
+		reversed = append(reversed, nums[i])
 	}
+	fmt.Println("Reversed slice:", reversed)
 }
